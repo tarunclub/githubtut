2d4f8aa5f457cdfc63401abd75488aab8f07105a
tarunclub <tarunkm__@outlook.com>
2024-02-12 18:18:20 +0530

    new msg

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.json({
+    success: true,
+  });
 });
 
 app.listen(3000, () => {
